cd25b9d38f3dfbc83c684884bbb477097c9da19e
Albert <albertsoares97@gmail.com>
2023-05-18 23:12:40 -0300

    ajustando filtros das tarefas

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,19 +1,53 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
+import { RootReducer } from '../../store'
+import { alterarTermo } from '../../store/reducers/filtro'
 
 import * as S from './styles'
+import * as enums from '../../utils/enums/Tarefa'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <S.Campo type="text" placeholder="Buscar" />
-    <S.Filtros>
-      <FiltroCard contador={12} legenda="pendentes" />
-      <FiltroCard contador={2} legenda="concluídas" />
-      <FiltroCard contador={3} legenda="urgentes" />
-      <FiltroCard contador={4} legenda="importantes" />
-      <FiltroCard contador={5} legenda="normal" />
-      <FiltroCard contador={14} legenda="todas" ativo />
-    </S.Filtros>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <S.Campo
+        type="text"
+        placeholder="Buscar"
+        value={termo}
+        onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+      />
+      <S.Filtros>
+        <FiltroCard
+          valor={enums.Status.PENDENTE}
+          criterio="status"
+          legenda="pendentes"
+        />
+        <FiltroCard
+          valor={enums.Status.CONCLUIDA}
+          criterio="status"
+          legenda="concluídas"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.URGENTE}
+          criterio="prioridade"
+          legenda="urgentes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.IMPORTANTE}
+          criterio="prioridade"
+          legenda="importantes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.NORMAL}
+          criterio="prioridade"
+          legenda="normal"
+        />
+        <FiltroCard criterio="todas" legenda="todas" />
+      </S.Filtros>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -2,37 +2,57 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefa'
 
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo orientação a objetos',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar Java Script'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar TypeScript revendo orientação a objetos',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar Type Script'
+    },
+    {
+      id: 3,
+      descricao: 'Estudar lógica de programação Python',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar Python'
+    }
+  ]
+}
+
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 2',
-      2
-    ),
-    new Tarefa(
-      'Estudar Python',
-      enums.Prioridade.NORMAL,
-      enums.Status.PENDENTE,
-      'Rever lógica de programação',
-      3
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = [
+        ...state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      ]
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
+
 export default tarefasSlice.reducer
